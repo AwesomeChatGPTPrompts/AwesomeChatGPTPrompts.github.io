6fa60419d2b42c25b4d98a7dcc6d3e50ec56db37
ZelunZhang <mylady-z@hotmail.com>
2023-04-08 10:18:26 +0100

    update

--- _next/static/P5drQYdLT_783dnbzLXgp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/508-adcfedd8de00a39a.js","static/css/81b78fb3a9e72703.css","static/chunks/pages/index-716d65962bac60ea.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
